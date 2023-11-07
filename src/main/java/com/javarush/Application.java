362110bf3682efdaa66675c1f96deda916a6efbc
Pavel Ivankov <ivankoffpavel@gmail.com>
2023-11-07 14:53:34 +0200

    2 commit

--- src/main/java/com/javarush/Application.java
+++ src/main/java/com/javarush/Application.java
@@ -3,6 +3,7 @@ package com.javarush;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("2 commit");
 
     }
 }
