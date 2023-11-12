0a92d326b79d7a518c9032a0bdd0b34eb3aa60ef
Pavel Ivankov <ivankoffpavel@gmail.com>
2023-11-12 21:41:39 +0200

    Scanners for command,filePath and key were written

--- src/main/java/com/javarush/cli/CLI.java
+++ src/main/java/com/javarush/cli/CLI.java
@@ -1,4 +1,12 @@
 package com.javarush.cli;
 
+import java.util.Scanner;
+
 public class CLI {
+    public void InputUser(){
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        String filePath = scanner.nextLine();
+        int key = scanner.nextInt();
+    }
 }
