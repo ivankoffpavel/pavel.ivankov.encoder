d021ba21331ef1189ae9c1104109d067e2e6af92
Pavel Ivankov <ivankoffpavel@gmail.com>
2023-11-12 12:17:31 +0200

    Class CesarCipher with approved algorithm was created

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,7 +1,15 @@
 package com.javarush;
 
+import com.javarush.cesarcipher.CesarCipher;
+
 public class Main {
     public static void main(String[] args) {
+        String text = "HI mon I am fine!";
+        int key = 5;
+        CesarCipher cesarCipher = new CesarCipher();
+        System.out.println(cesarCipher.CesarEncryptor(text,key));
+        String Encrypted = cesarCipher.CesarEncryptor(text,key).toString();
+        System.out.println(cesarCipher.CesarDecryptor(Encrypted,key));
 
 
     }
--- src/main/java/com/javarush/cesarcipher/CesarCipher.java
+++ src/main/java/com/javarush/cesarcipher/CesarCipher.java
@@ -1,9 +1,31 @@
 package com.javarush.cesarcipher;
 
-public class CesarCipher {
-    int key;
-    String inputText;
-    public StringBuilder CesarEncryptor(String inputText,int key){
+import com.javarush.constants.AlphabetSamples;
 
+public class CesarCipher {
+
+    public StringBuilder CesarEncryptor(String inputText, int key) {
+        char[] inputTextCharArray = inputText.toCharArray();
+        StringBuilder encryptedText = new StringBuilder();
+        for (int i = 0; i < inputTextCharArray.length; i++) {
+            if (AlphabetSamples.EN_ALPHABET.contains(inputTextCharArray[i])) {
+                int currentIndexChar = AlphabetSamples.EN_ALPHABET.indexOf(inputTextCharArray[i]);
+                encryptedText.append(AlphabetSamples.EN_ALPHABET.get((currentIndexChar + key) % AlphabetSamples.EN_ALPHABET.size()));
+            }
+        }
+        return encryptedText;
+    }
+
+    public StringBuilder CesarDecryptor(String inputText, int key) {
+        char[] inputTextCharArrayArray = inputText.toCharArray();
+        StringBuilder decryptedText = new StringBuilder();
+        for (int i = 0; i < inputTextCharArrayArray.length; i++) {
+            if (AlphabetSamples.EN_ALPHABET.contains(inputTextCharArrayArray[i])) {
+                int currentIndexChar = AlphabetSamples.EN_ALPHABET.indexOf(inputTextCharArrayArray[i]);
+                int currentIndexCharMinusKey = currentIndexChar > key ? currentIndexChar - key : AlphabetSamples.EN_ALPHABET.size() - (key - currentIndexChar);
+                decryptedText.append(AlphabetSamples.EN_ALPHABET.get(currentIndexCharMinusKey));
+            }
+        }
+        return decryptedText;
     }
 }
