eb642e6bc2dc77495ace6e4db7288fc6a851c2df
Pavel Ivankov <ivankoffpavel@gmail.com>
2023-11-12 16:40:23 +0200

    Class FileService was written and approved in work

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,11 +1,16 @@
 package com.javarush;
 
 import com.javarush.cesarcipher.CesarCipher;
+import com.javarush.io.FileService;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "HI mon I am fine!";
+        String text = "HI mon I am fine*";
         int key = 5;
+        FileService fileService = new FileService("C:/Java/1.txt");
+        String str = fileService.readFile();
+        System.out.println(str);
+        fileService.writeFile(text);
         CesarCipher cesarCipher = new CesarCipher();
         System.out.println(cesarCipher.CesarEncryptor(text,key));
         String Encrypted = cesarCipher.CesarEncryptor(text,key).toString();
---
+++ src/main/java/com/javarush/io/FileService.java
@@ -0,0 +1,46 @@
+package com.javarush.io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileService {
+    private String filePath;
+
+
+    public FileService(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String readFile() {
+        StringBuilder readString = new StringBuilder();
+        try(FileInputStream fis = new FileInputStream(filePath)) {
+            int i;
+            while ((i = fis.read())!=-1){
+                readString.append((char) i);
+            }
+
+        } catch (FileNotFoundException ex){
+            System.out.println("File not found!");
+        } catch (IOException e){
+            System.out.println("Oops! "+e.getMessage());
+        }
+        return readString.toString();
+    }
+    public String writeFile(String encryptedText){
+        try(OutputStream fos = Files.newOutputStream(Paths.get(filePath+"[ENCRYPTED]"))){
+            byte[] bytes = encryptedText.getBytes();
+            fos.write(bytes);
+        }catch (IOException  ex){
+            System.out.println("Oops! "+ex.getMessage());
+
+        }
+        return "";
+    }
+
+}
+
+
